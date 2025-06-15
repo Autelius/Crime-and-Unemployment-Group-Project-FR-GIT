19a61452c5a1781174585cfcf6eec988ee8d4f7f
Romeo927 <romeoschoonderbeek@gmail.com>
2025-06-15 19:10:24 +0200

    added per capita variable and more data cleaning

--- code/data_cleaning_for_aggragate.R
+++ code/data_cleaning_for_aggragate.R
@@ -21,4 +21,46 @@ colnames(agg_crime) = c("year", "total_crimes")
 
 agg_merged <- cbind(agg_unemployment, agg_crime[, 2])
 
+#additional cleaning to make analysis easier
+
+colnames(agg_merged) = c("year", "unemployment", "total_crime")
+
+agg_merged <- agg_merged %>%
+  mutate(total_crime = as.numeric(total_crime)) %>%
+  mutate(year = as.numeric(year))
+
+#use the following comand to check if all numerics are actually numerics
+# and not characters, which is vital for the plots
+
+str(agg_merged)
+
+#importing raw population data
+
+agg_population <- read.csv("data/raw_data/population_data_raw.csv")
+
+#cleaning population data
+
+agg_population <-agg_population[, -4]
+agg_population <- agg_population[, -1]
+agg_population <- agg_population[-c(1:75) ,]
+
+#merging agg_pop to agg_merged
+
+agg_merged <- cbind(agg_merged, agg_population[, 2])
+
+#readjusting names
+
+colnames(agg_merged) = c("year", "unemployment", "total_crime", "population")
+
+# just like once before, use this command to see if everything is classed as numeric
+str(agg_merged)
+
+##adding per capita variable
+
+agg_merged <- agg_merged %>%
+  mutate(crime_per_capita = (total_crime/population)*100)
+
+
+
+
 
