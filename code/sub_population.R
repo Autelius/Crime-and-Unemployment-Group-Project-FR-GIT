e0c585cbb8d750a7eb834645cdb8e17bb491623f
Romeo927 <romeoschoonderbeek@gmail.com>
2025-06-19 11:44:32 +0200

    sub population code finished

--- code/sub_population.R
+++ code/sub_population.R
@@ -28,4 +28,32 @@ ggplot(crime2021, aes(x = leeftijdcat, y = crime)) +
     y = "Suspects per 10,000 Inhabitants"
   ) 
 
+##sub population analysis for unemployment
+
+unemployment_age <- read.csv("data/raw_data/unemployment_per_age.csv")
+
+unemployment_age <- unemployment_age[-c(1:6, 19) ,]
+
+unemployment_age <- unemployment_age[, -1]
+
+colnames(unemployment_age) = c("leeftijd", "unemployment")
+
+unemployment_age$leeftijd <- ifelse(unemployment_age$leeftijd %in% c("15 tot 20 jaar", "20 tot 25 jaar"),
+                             "Youth", "Adult")
+
+unemployment_age$unemployment <- gsub(",", ".", unemployment_age$unemployment)
+
+# Convert character to numeric
+unemployment_age$unemployment <- as.numeric(unemployment_age$unemployment)
+
+ggplot(unemployment_age, aes(x = leeftijd, y = unemployment)) +
+  geom_boxplot(fill = "skyblue", color = "darkblue") +
+  labs(
+    title = "Boxplot of Crime Suspect Rate by Age Group",
+    x = "Grouped Age Range",
+    y = "unemployment"
+  ) 
+
+
+
 
