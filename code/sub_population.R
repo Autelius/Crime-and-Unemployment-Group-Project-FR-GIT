59f5684547eab11f9813a7ffd8c767273ceda5f5
Romeo927 <romeoschoonderbeek@gmail.com>
2025-06-24 15:23:28 +0200

    subopulation finished in r markdown and debugged

--- code/sub_population.R
+++ code/sub_population.R
@@ -4,28 +4,25 @@ crime2021 <- criminaliteit_per_leeftijd %>% filter(Perioden == "2021")
 
 crime2021 <- crime2021 %>% filter(Geboorteland == "Totaal")
 
-columns <- c("Leeftijdscategorie", "PercCriminilateit")
+crime2021 <- crime2021[, -c(1:2, 4:8)]
 
-eindproduct <- columns
+colnames(crime2021) <- c("Age", "Crime")
 
-crime2021 <- crime2021[, -c(1:2)]
-crime2021 <- crime2021[, -c(2:6)]
+crime2021$Crime = (crime2021$Crime/10000)*100
 
 #making categories
 
-crime2021$Leeftijd <- ifelse(crime2021$Leeftijd %in% c("12 tot 18 jaar", "18 tot 23 jaar"),
+crime2021$Age <- ifelse(crime2021$Age %in% c("12 tot 18 jaar", "18 tot 23 jaar"),
                                      "Youth (12–23)", "Adult+ (23+)")
-
-colnames(crime2021) = c("leeftijdcat", "crime")
 
 # making boxplot
 # Create the boxplot
-ggplot(crime2021, aes(x = leeftijdcat, y = crime)) +
+ggplot(crime2021, aes(x = Age, y = Crime)) +
   geom_boxplot(fill = "skyblue", color = "darkblue") +
   labs(
     title = "Boxplot of Crime Suspect Rate by Age Group",
-    x = "Grouped Age Range",
-    y = "Suspects per 10,000 Inhabitants"
+    x = "Age category",
+    y = "Crime Rate"
   ) 
 
 ##sub population analysis for unemployment
@@ -36,23 +33,27 @@ unemployment_age <- unemployment_age[-c(1:6, 19) ,]
 
 unemployment_age <- unemployment_age[, -1]
 
-colnames(unemployment_age) = c("leeftijd", "unemployment")
+colnames(unemployment_age) = c("Age", "Unemployment")
 
-unemployment_age$leeftijd <- ifelse(unemployment_age$leeftijd %in% c("15 tot 20 jaar", "20 tot 25 jaar"),
-                             "Youth", "Adult")
+unemployment_age$Unemployment <- gsub(",", ".", unemployment_age$Unemployment)
 
-unemployment_age$unemployment <- gsub(",", ".", unemployment_age$unemployment)
+unemployment_age$Unemployment <- as.numeric(unemployment_age$Unemployment)
+
+
+unemployment_age$Age <- ifelse(unemployment_age$Age %in% c("15 tot 20 jaar", "20 tot 25 jaar"),
+                             "Youth (15-25)", "Adult (25+)")
 
 # Convert character to numeric
-unemployment_age$unemployment <- as.numeric(unemployment_age$unemployment)
+unemployment_age$Unemployment <- as.numeric(unemployment_age$Unemployment)
 
-ggplot(unemployment_age, aes(x = leeftijd, y = unemployment)) +
+ggplot(unemployment_age, aes(x = Age, y = Unemployment)) +
   geom_boxplot(fill = "skyblue", color = "darkblue") +
   labs(
     title = "Boxplot of Crime Suspect Rate by Age Group",
-    x = "Grouped Age Range",
+    x = "Age category",
     y = "unemployment"
   ) 
+
 
 
 
